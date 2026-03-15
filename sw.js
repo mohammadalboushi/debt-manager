293e920f22a1f9c4644d5fe878dbac6ea15a6b86
malboushi <mohammad.alaa.love.619@gmail.com>
2026-03-15 20:11:13 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,21 @@
-const CACHE_NAME = 'note-v3';
-const urlsToCache = [
-  './',
-  './index.html',
-  './style.css',
-  './script.js',
-  './icon.png'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
+const CACHE_NAME = 'debt-v1';
+const urlsToCache = [
+  'index.html',
+  'style.css',
+  'script.js',
+  'icon.png',
+  'manifest.json'
+];
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
 });
